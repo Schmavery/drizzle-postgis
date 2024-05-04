e24ed516edf36770cb27075445f186fb6f8ebefd
Avery Morin <avery.schmavery@gmail.com>
2024-05-04 16:39:45 -0400

    Fix CI (#3)

--- globalSetup.ts
+++ globalSetup.ts
@@ -1,12 +1,24 @@
 import { execSync } from "child_process";
 
+let startedContainers = false;
 export async function setup() {
-  console.log("Starting up test DB...");
-  execSync("docker compose --progress=quiet up --wait");
-  console.log("Started docker compose!");
+  const res = execSync("docker ps", { encoding: "utf-8" });
+  console.log(res);
+  const containerRunning =
+    res.includes("postgis") || res.includes("act-CI-test");
+  if (!containerRunning) {
+    console.log("Starting up test DB...");
+    execSync("docker compose up --wait", { encoding: "utf-8" });
+    startedContainers = true;
+    console.log("Started docker compose!");
+  }
 }
 
 export function teardown() {
-  execSync("docker compose --progress=quiet down --volumes");
-  console.log("Cleaned up docker compose!");
+  if (startedContainers) {
+    console.log("Cleaned up docker compose!");
+    execSync("docker compose --progress=quiet down --volumes", {
+      encoding: "utf-8",
+    });
+  }
 }
--- src/test/test-helpers.ts
+++ src/test/test-helpers.ts
@@ -18,7 +18,10 @@ export function expectToBeDefined<T>(
 }
 
 export function makeDB() {
-  const client = postgres(process.env.DRIZZLE_DB_URL!);
+  const client = postgres(
+    process.env.DRIZZLE_DB_URL ??
+      "postgresql://postgres:example@127.0.0.1:5433/postgres"
+  );
   return drizzle(client, { schema });
 }
 
@@ -27,13 +30,13 @@ const db = makeDB();
 export function rollbackTest(
   name: string,
   cb: (
-    ctx: TaskContext<Test<{}>> &
+    ctx: TaskContext<Test<unknown>> &
       TestContext & { db: PostgresJsDatabase<typeof schema> }
   ) => Promise<unknown>
 ) {
   test(name, (ctx) =>
     expect(
-      async () =>
+      (async () =>
         await db.transaction(async (db) => {
           await cb({
             db: db,
@@ -44,7 +47,7 @@ export function rollbackTest(
             task: ctx.task,
           });
           db.rollback();
-        })
+        }))()
     ).rejects.toThrowError("Rollback")
   );
 }
