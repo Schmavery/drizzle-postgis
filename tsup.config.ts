5b1960c5146f63f27b4d80c69247e542d77c9437
Nicholas Dobie <nrdobie@users.noreply.github.com>
2024-06-17 15:44:14 -0500

    chore(package.json): :technologist: Added more export endpoints to make importing cleaner. (#11)
    * chore(package.json): :technologist: Added more export endpoints to make importing cleaner.
    
    Updated the build tools to bundle `functions`, `models`, and `operations` as seperate exports to make importing and using easier. Developers will now be able to do `import { point } from 'drizzle-postgis/models` rather than having to do `import { models } from 'drizzle-postgis'` and then calling `models.point`.  Also turned of IIFE builds as they were unused.
    
    * Update version to 1.1.1
    
    ---------
    
    Co-authored-by: Avery Morin <avery.schmavery@gmail.com>

--- tsup.config.ts
+++ tsup.config.ts
@@ -7,9 +7,12 @@ export default defineConfig(({ watch = false }) => ({
   dts: true,
   entry: {
     index: 'src/index.ts',
+    functions: 'src/functions.ts',
+    models: 'src/models.ts',
+    operators: 'src/operators.ts'
   },
   external: [],
-  format: ['cjs', 'esm', 'iife'],
+  format: ['cjs', 'esm'],
   treeshake: isProduction,
   minify: isProduction,
   sourcemap: isProduction,
